60845d975547ce532444d9ad2efe1332c5f66ee7
Yohance <ypawania@gmail.com>
2024-02-25 21:53:32 -0500

    Improved Serial Read Speed
    Remove ParseInt.
    Manually read bytes and move servo position

--- config-tune/config-tune.ino
+++ config-tune/config-tune.ino
@@ -1,18 +1,52 @@
 #include <ESP32Servo.h>
-Servo myservo;  
+Servo Xservo;  
 int pos = 0;   
+boolean startbit = false;
+boolean newlinerecieved = false;
+String inputString = "";
+String recieved_data = "";
+int incomingByte = 0;
+void on_recieve();
+String motor;
+int angle;
 
 void setup() {
-	myservo.setPeriodHertz(50);    
-	myservo.attach(18, 500, 2400); 
+	Xservo.setPeriodHertz(50);    
+	Xservo.attach(18, 500, 2400); 
   Serial.begin(9600);
 }
 
+
 void loop() {
   if (Serial.available() > 0){
-    pos = Serial.parseInt();
+    incomingByte = Serial.read();
+    if (incomingByte == '%'){
+      startbit = true;
+    }
+    if (startbit == true){
+      inputString += (char) incomingByte;
+    }
+    if (startbit == true && incomingByte == '#'){
+      newlinerecieved = true;
+      startbit = false;
+    }
+    if (newlinerecieved == true){
+      recieved_data = inputString.substring(1, (inputString.length() - 1));
+      inputString = "";
+      newlinerecieved = false;
+      on_recieve();
+    }
+
   }  
-  
-  myservo.write(pos);
-  delay(200);
 }
+void on_recieve(){
+    motor = recieved_data.substring(0,1);
+    angle = recieved_data.substring(1, recieved_data.length()).toInt();
+
+    if (motor = "X"){
+      Xservo.write(angle);
+      delay(100);
+    }
+    
+  }
+
--- config.py
+++ config.py
@@ -1,12 +1,26 @@
 import cv2
 import numpy as np
+import serial
+
+left = True
+right, up, down = False, False, False
 
 cam = cv2.VideoCapture(0)
+ser = serial.Serial(port='COM8', baudrate=9600)
 
 while True:
     ret, frame = cam.read()
-    frame = cv2.line(frame, (10, 450), (10, 10), (0, 80, 0), 5)
-    cv2.imshow('Left Frame',frame)
+    frame = cv2.rectangle(frame, (10,10), (620, 450), (0,80,0), 5)
+    cv2.imshow('frame', frame)
+    
+    new = input("Enter Servo Angle: ")
+    if (new != 'done'):
+        old = new
+        ser.write(('%'+ 'X'+(str(new)) + '#').encode())
+    elif (new == 'done'):
+        leftAngle = old
+        
     if cv2.waitKey(1) & 0xff ==ord('q'):
         break
 cam.release()
+ser.close()
