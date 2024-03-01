63e23e905ff2f5fdfc932f07c482642de2811337
Yohance <ypawania@gmail.com>
2024-02-29 20:45:25 -0500

    Completed config function
    Implemented logic. Works. Needs cleanup / refactoring.

--- config.py
+++ config.py
@@ -2,23 +2,52 @@ import cv2
 import numpy as np
 import serial
 
-left = True
-right, up, down = False, False, False
+##make dictionary for left, right, up, down with bools and angles
+##iterate through, and repeat for directions till they are all true 
+
+leftAngle, rightAngle, upAngle, downAngle = 0, 0, 0, 0
+old = 0
+angleNumber = 0
+## 0 = left; 1 = right; 2 = up; 3 = down;
+
 
 cam = cv2.VideoCapture(0)
 ser = serial.Serial(port='COM8', baudrate=9600)
 
-while True:
-    ret, frame = cam.read()
-    frame = cv2.rectangle(frame, (10,10), (620, 450), (0,80,0), 5)
-    cv2.imshow('frame', frame)
-    
+def config():
+    global leftAngle, rightAngle, upAngle, downAngle, angleNumber, old
     new = input("Enter Servo Angle: ")
     if (new != 'done'):
+        ret, frame = cam.read()
+        frame = cv2.rectangle(frame, (10,10), (620, 450), (0,80,0), 5)
+        cv2.imshow('frame', frame)
         old = new
-        ser.write(('%'+ 'X'+(str(new)) + '#').encode())
+        if (angleNumber == 0 or angleNumber == 1):
+            axis = 'X'
+        elif (angleNumber == 2 or angleNumber == 3):
+            axis = 'Y'
+        ser.write(('%'+axis+(str(new)) + '#').encode())
+    
     elif (new == 'done'):
-        leftAngle = old
+        match angleNumber:
+            case 0:
+                leftAngle = old
+                print('left angle = ', leftAngle)
+            case 1:
+                rightAngle = old
+                print('rightangle = ', rightAngle)
+            case 2:
+                upAngle = old
+            case 3:
+                downAngle = old
+            case _:
+                print("this should never happen. bad bad bad- ")
+        angleNumber += 1
+        
+
+while angleNumber < 4:
+    
+    config()
         
     if cv2.waitKey(1) & 0xff ==ord('q'):
         break
